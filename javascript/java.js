5aef0bf486d7b0e025af44cbd898e4b23bb5f2bb
David Ekai <100727132+davidkantel@users.noreply.github.com>
2022-11-11 10:32:08 +0300

    Add files via upload

---
+++ javascript/java.js
@@ -0,0 +1,33 @@
+document.getElementById('welcome').style.color="green";
+let x=20;
+let y=20;
+let c= x + y;
+console.log(c);
+document.getElementById('ekai').innerHTML= "The students who will go to the show will amount to :" + c + " only";
+// function david(){
+//     document.getElementById('ekai').innerHTML="Woooooop !!!! Ekai congratulation you have qualified for Loan application with  limit of 30,000"
+// }
+document.getElementById('ekai').style.color='red';
+let person='ekai', carname='prado', price=800000;
+let message="On friday morning " + person + " went and bought a " + carname + " which cost him " + price;
+document.getElementById('ekai').innerHTML=(message);
+var d ="come home on monday";
+var b= "and make sure to do what we had agreed"
+console.log(d+=" morning")
+console.log(message)
+const car={
+    type:"prado",
+    price:50000,
+    model:"5000",
+    color:"green"
+};
+let info="I have a  " + car.type + "  i will decide to add another "
+console.log(car.type);
+console.log(car.price);
+console.log(car.color);
+console.log(info);
+statement:function ekai(){
+    return this.car.color
+}
+
+
